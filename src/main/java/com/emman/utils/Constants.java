8b4fce0a3f93a2e679f4556d4b0026aaa750b59a
Emmanuel Utomi <emmanuelutomi@gmail.com>
2015-06-19 23:13:48 -0500

    AppUtils: Initial Commit

---
+++ src/main/java/com/emman/utils/Constants.java
@@ -0,0 +1,16 @@
+package com.emman.utils;
+
+import android.os.Environment;
+
+public interface Constants {
+
+	//Global Variables and stored data
+
+	public static final String FILE_TEXT_FORMAT = "text/*";
+	public static final String EXTRA_FORCE_SHOW_LIGHTS = "android.forceShowLights"; //supported by most custom AOSP ROMs
+
+	public static final String NEW_LINE = "\n";
+	public static final String LINE_SPACE = " ";
+
+	public static final String NUM_OF_CPUS_PATH = "/sys/devices/system/cpu/present";
+}
---
+++ src/main/java/com/emman/utils/NotificationID.java
@@ -0,0 +1,12 @@
+package com.emman.utils;
+
+public enum NotificationID {
+	EXTRACT,
+	BLN_TEST,
+	ROOTFAIL,
+	CPUPERM,
+	UID,
+	APPUPDATE,
+	WKUPDATE,
+	DSOB,
+}
